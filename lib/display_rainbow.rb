7fefd9ce1048cf34749a35c7b4eee98a750b7b75
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-11-19 18:55:44 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,6 @@
 # Write your #display_rainbow method here
+def display_rainbow(argument)
+  puts R: argument[0], O: argument[1], Y: argument[2], G: argument[3], B: argument[4], I: argument[5], V: argument[6] 
+end
+ 
+  
